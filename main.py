f9725d72bab822c43c60b4be7a0e0d99e8d36372
rravada <roshan.ravada@gmail.com>
2023-06-22 13:52:57 -0700

    Assignment2-1

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    nomales = int(input('Enter the amount of males registered in the class: '))
+    nofemales =int(input('Enter the amount of females registed in the class: '))
+    total = nomales + nofemales
+    m_perc = (nomales/total)*100
+    f_perc = (nofemales/total)*100
+    print('Total number of students: ', total)
+    print('Total number of males and females: ', nomales, "\t", nofemales)
+    print('The percentage of males and females', m_perc, "% \t", f_perc, "%")
     """
     ########################################
     # Do not delete the return statement
